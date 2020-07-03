ea67d71dcab03820ddd6f01675edf3807e859fda
gurman-crypto <gurmandeep94@gmail.com>
2020-07-03 15:54:58 -0400

    first commit

---
+++ dashboards/src/main/java/dashboards/AppStartup.java
@@ -0,0 +1,15 @@
+package dashboards;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStartup.class, args);
+	}
+
+}
+
